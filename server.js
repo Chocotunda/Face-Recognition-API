b0ab271d5bc9065da16eabc10f7b0c2345c4c7b9
Chocotunda <kdgganpat@gmail.com>
2019-04-10 00:16:43 +0200

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-octagonal-56631',
-    user: 'krishen',
-    password: '',
-    database: 'face-recognition'
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
