c6abb8bf8ac43c077f23cd6ff0eac6ac14e25054
Chocotunda <kdgganpat@gmail.com>
2019-04-09 21:51:46 +0200

    added validation + api key in server

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '4856bb19a4054b42830a09e249307f5b'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('unable to work with API'));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
@@ -11,5 +26,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-  handleImage
+  handleImage,
+  handleApiCall
 };
--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.put('/image', (req, res) => {
   image.handleImage(req, res, db);
 });
 
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
+
 app.listen(3000, () => {
   console.log('server is running on port 3000');
 });
