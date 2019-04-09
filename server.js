a637d82681610d6223175c1336dd858b6a678c5d
Chocotunda <kdgganpat@gmail.com>
2019-04-10 00:53:04 +0200

    bugfix database

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const db = knex({
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('it is working!');
