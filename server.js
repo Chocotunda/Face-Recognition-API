927ee557df845f704ad1e69dbd964ad11f178d7e
Chocotunda <kdgganpat@gmail.com>
2019-04-10 00:14:21 +0200

    changed db to heroku db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-octagonal-56631',
     user: 'krishen',
     password: '',
     database: 'face-recognition'
