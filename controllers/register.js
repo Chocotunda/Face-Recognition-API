d507caf95132b849997ae3bf14c07223f52bd608
Chocotunda <kdgganpat@gmail.com>
2019-04-10 01:05:37 +0200

    bugfix register

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
-    return res.status(400).json('Incorrect form submission')
+    return res.status(400).json('Incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
   db.transaction((trx) => {
