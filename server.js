bc1cf949e47655c3dbaa16d2c21454ca52b93527
Chocotunda <kdgganpat@gmail.com>
2019-04-09 17:42:38 +0200

    signing working with db

--- server.js
+++ server.js
@@ -45,14 +45,25 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json('Unable to get user'));
+      } else {
+        res.status(400).json('Wrong credentials');
+      }
+    })
+    .catch((err) => res.status(400).json('Wrong credentials'));
 });
 
 app.post('/register', (req, res) => {
