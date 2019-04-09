47c964b27e9f63d18c500e5c96ad08928b7984a7
Chocotunda <kdgganpat@gmail.com>
2019-04-09 17:43:14 +0200

    removed dummy database

--- server.js
+++ server.js
@@ -16,27 +16,6 @@ const db = knex({
 
 const app = express();
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-};
-
 app.use(bodyParser.json());
 app.use(cors());
 
